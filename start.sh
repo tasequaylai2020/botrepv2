092c9562c1895931170236e46a52c475527fec01
Nguyễn Quang Trọng <tasequaylai2020@gmail.com>
2021-11-29 22:34:34 +0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Nhân bản chi nhánh chính...."
-  git clone https://github.com/gimsuri69/VideoPlayerTG /VideoPlayerTG
+  git clone https://github.com/tasequaylai2020/botrepv2 /VideoPlayerTG
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/gimsuri69/VideoPlayerTG -b $BRANCH /VideoPlayerTG
+  git clone https://github.com/tasequaylai2020/botrepv2 -b $BRANCH /VideoPlayerTG
 fi
 cd /VideoPlayerTG
 pip3 install -U -r requirements.txt
