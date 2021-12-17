4c335679c1996e66cd8628d95eb999ef17dc81b7
Nguyễn Quang Trọng <tasequaylai2020@gmail.com>
2021-12-17 19:02:54 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/tasequaylai2020/botrepv2 /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/tasequaylai2020/botrepv2 -b $BRANCH /VCPlayerBot
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
